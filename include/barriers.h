f9d53a59225b82a7df36e94f5a5cfcd49087ca9b
jhbdream <1023506846@qq.com>
2021-07-18 10:53:56 +0800

    [update] io.h barriers.h - change from linux source

--- include/barriers.h
+++ include/barriers.h
@@ -1,12 +1,27 @@
 #ifndef __BARRIERS_H__
 #define __BARRIERS_H__
 
-#define ISB	asm volatile ("isb sy" : : : "memory")
-#define DSB	asm volatile ("dsb sy" : : : "memory")
-#define DMB	asm volatile ("dmb sy" : : : "memory")
+#define __nops(n)	".rept	" #n "\nnop\n.endr\n"
+#define nops(n)		asm volatile(__nops(n))
 
-#define isb()	ISB
-#define dsb()	DSB
-#define dmb()	DMB
+#define sev()		asm volatile("sev" : : : "memory")
+#define wfe()		asm volatile("wfe" : : : "memory")
+#define wfi()		asm volatile("wfi" : : : "memory")
+
+#define isb()		asm volatile("isb" : : : "memory")
+#define dmb(opt)	asm volatile("dmb " #opt : : : "memory")
+#define dsb(opt)	asm volatile("dsb " #opt : : : "memory")
+
+#define psb_csync()	asm volatile("hint #17" : : : "memory")
+#define tsb_csync()	asm volatile("hint #18" : : : "memory")
+#define csdb()		asm volatile("hint #20" : : : "memory")
+
+#define mb()		dsb(sy)
+#define rmb()		dsb(ld)
+#define wmb()		dsb(st)
+
+#define dma_mb()	dmb(osh)
+#define dma_rmb()	dmb(oshld)
+#define dma_wmb()	dmb(oshst)
 
 #endif	/* __BARRIERS_H__ */
