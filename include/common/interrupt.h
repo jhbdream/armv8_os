55c50faf199f505b7910e2c5731eb98539f0a545
jhbdream <1023506846@qq.com>
2021-12-27 00:16:17 +0800

    [update] interrupt - add irq save/restore interface - fix irq enable/disable

--- include/common/interrupt.h
+++ include/common/interrupt.h
@@ -9,24 +9,39 @@
 
 #define NR_IRQS 8192
 
-#define local_irq_enable()				\
-	do {						\
-		arch_local_irq_enable();			\
-	} while (0)
+#define local_irq_enable()       \
+    do                           \
+    {                            \
+        arch_local_irq_enable(); \
+    } while (0)
 
-#define local_irq_disable()				\
-	do {						\
-		arch_local_irq_disable();		\
-	} while (0)
+#define local_irq_disable()       \
+    do                            \
+    {                             \
+        arch_local_irq_disable(); \
+    } while (0)
+
+#define local_irq_save(flags)          \
+    do                                 \
+    {                                  \
+        flags = arch_local_irq_save(); \
+    } while (0)
+
+#define local_irq_restore(flags)       \
+    do                                 \
+    {                                  \
+        arch_local_irq_restore(flags); \
+    } while (0)
 
 /*
  * irq_get_irqchip_state/irq_set_irqchip_state specific flags
  */
-enum irqchip_irq_state {
-	IRQCHIP_STATE_PENDING,		/* Is interrupt pending? */
-	IRQCHIP_STATE_ACTIVE,		/* Is interrupt in progress? */
-	IRQCHIP_STATE_MASKED,		/* Is interrupt masked? */
-	IRQCHIP_STATE_LINE_LEVEL,	/* Is IRQ line high? */
+enum irqchip_irq_state
+{
+    IRQCHIP_STATE_PENDING,    /* Is interrupt pending? */
+    IRQCHIP_STATE_ACTIVE,     /* Is interrupt in progress? */
+    IRQCHIP_STATE_MASKED,     /* Is interrupt masked? */
+    IRQCHIP_STATE_LINE_LEVEL, /* Is IRQ line high? */
 };
 
 /*
@@ -71,35 +86,36 @@ enum irqchip_irq_state {
  *				  mechanism and from core side polling.
  * IRQ_DISABLE_UNLAZY		- Disable lazy irq disable
  */
-enum {
-	IRQ_TYPE_NONE		= 0x00000000,
-	IRQ_TYPE_EDGE_RISING	= 0x00000001,
-	IRQ_TYPE_EDGE_FALLING	= 0x00000002,
-	IRQ_TYPE_EDGE_BOTH	= (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING),
-	IRQ_TYPE_LEVEL_HIGH	= 0x00000004,
-	IRQ_TYPE_LEVEL_LOW	= 0x00000008,
-	IRQ_TYPE_LEVEL_MASK	= (IRQ_TYPE_LEVEL_LOW | IRQ_TYPE_LEVEL_HIGH),
-	IRQ_TYPE_SENSE_MASK	= 0x0000000f,
-	IRQ_TYPE_DEFAULT	= IRQ_TYPE_SENSE_MASK,
+enum
+{
+    IRQ_TYPE_NONE = 0x00000000,
+    IRQ_TYPE_EDGE_RISING = 0x00000001,
+    IRQ_TYPE_EDGE_FALLING = 0x00000002,
+    IRQ_TYPE_EDGE_BOTH = (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING),
+    IRQ_TYPE_LEVEL_HIGH = 0x00000004,
+    IRQ_TYPE_LEVEL_LOW = 0x00000008,
+    IRQ_TYPE_LEVEL_MASK = (IRQ_TYPE_LEVEL_LOW | IRQ_TYPE_LEVEL_HIGH),
+    IRQ_TYPE_SENSE_MASK = 0x0000000f,
+    IRQ_TYPE_DEFAULT = IRQ_TYPE_SENSE_MASK,
 
-	IRQ_TYPE_PROBE		= 0x00000010,
+    IRQ_TYPE_PROBE = 0x00000010,
 
-	IRQ_LEVEL		= (1 <<  8),
-	IRQ_PER_CPU		= (1 <<  9),
-	IRQ_NOPROBE		= (1 << 10),
-	IRQ_NOREQUEST		= (1 << 11),
-	IRQ_NOAUTOEN		= (1 << 12),
-	IRQ_NO_BALANCING	= (1 << 13),
-	IRQ_MOVE_PCNTXT		= (1 << 14),
-	IRQ_NESTED_THREAD	= (1 << 15),
-	IRQ_NOTHREAD		= (1 << 16),
-	IRQ_PER_CPU_DEVID	= (1 << 17),
-	IRQ_IS_POLLED		= (1 << 18),
-	IRQ_DISABLE_UNLAZY	= (1 << 19),
+    IRQ_LEVEL = (1 << 8),
+    IRQ_PER_CPU = (1 << 9),
+    IRQ_NOPROBE = (1 << 10),
+    IRQ_NOREQUEST = (1 << 11),
+    IRQ_NOAUTOEN = (1 << 12),
+    IRQ_NO_BALANCING = (1 << 13),
+    IRQ_MOVE_PCNTXT = (1 << 14),
+    IRQ_NESTED_THREAD = (1 << 15),
+    IRQ_NOTHREAD = (1 << 16),
+    IRQ_PER_CPU_DEVID = (1 << 17),
+    IRQ_IS_POLLED = (1 << 18),
+    IRQ_DISABLE_UNLAZY = (1 << 19),
 };
 
 struct irq_desc;
-typedef	void (*irq_handler_t)(struct irq_desc *desc);
+typedef void (*irq_handler_t)(struct irq_desc *desc);
 
 /**
  * struct irq_desc - interrupt descriptor
@@ -111,29 +127,30 @@ typedef	void (*irq_handler_t)(struct irq_desc *desc);
  * @irq_count:		stats field to detect stalled irqs
  * @name:		flow handler name for /proc/interrupts output
  */
-struct irq_desc {
-	irq_handler_t	handle_irq;
-	unsigned int 		irq;
-	unsigned int 		flags;
-	unsigned int		status_use_accessors;
-	unsigned int		depth;		/* nested irq disables */
-	unsigned int		irq_count;	/* For detecting broken IRQs */
-	struct cpumask		*percpu_enabled;
-	const struct cpumask	*percpu_affinity;
-	const char		*name;
-	void			*handler_data;
+struct irq_desc
+{
+    irq_handler_t handle_irq;
+    unsigned int irq;
+    unsigned int flags;
+    unsigned int status_use_accessors;
+    unsigned int depth;     /* nested irq disables */
+    unsigned int irq_count; /* For detecting broken IRQs */
+    struct cpumask *percpu_enabled;
+    const struct cpumask *percpu_affinity;
+    const char *name;
+    void *handler_data;
 };
 
 extern struct irq_desc irq_desc[NR_IRQS];
 
 static inline unsigned int irq_desc_get_irq(struct irq_desc *desc)
 {
-	return desc->irq;
+    return desc->irq;
 }
 
-void handle_domain_irq( struct pt_regs *regs);
+void handle_domain_irq(struct pt_regs *regs);
 
 int request_irq(unsigned int irq, irq_handler_t handler, unsigned long flags,
-					const char *name, void *data);
+                const char *name, void *data);
 
 #endif
--- libcpu/aarch64/include/irqflags.h
+++ libcpu/aarch64/include/irqflags.h
@@ -6,12 +6,38 @@
  */
 static inline void arch_local_irq_enable(void)
 {
-	asm volatile( "msr daifclr, #3" ::: "memory");
+    asm volatile("msr daifclr, #2" ::
+                     : "memory");
 }
 
 static inline void arch_local_irq_disable(void)
 {
-	asm volatile( "msr daifset, #3" ::: "memory");
+    asm volatile("msr daifset, #2" ::
+                     : "memory");
+}
+
+static inline unsigned long arch_local_irq_save(void)
+{
+    unsigned long flags;
+    asm volatile(
+        "mrs	%0, daif		// arch_local_irq_save\n"
+        "msr	daifset, #2"
+        : "=r"(flags)
+        :
+        : "memory");
+    return flags;
+}
+
+/*
+ * restore saved IRQ state
+ */
+static inline void arch_local_irq_restore(unsigned long flags)
+{
+    asm volatile(
+        "msr	daif, %0		// arch_local_irq_restore"
+        :
+        : "r"(flags)
+        : "memory");
 }
 
 #endif
