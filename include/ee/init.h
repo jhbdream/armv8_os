72d38b9893cc8dd234e5041c68369049e36be433
jihongbin <longma@orbbec.com>
2022-04-07 17:38:15 +0800

    [update]: 添加setup_arch接口

---
+++ include/ee/init.h
@@ -0,0 +1,6 @@
+#ifndef _EE_INIT_H_
+#define _EE_INIT_H_
+
+void setup_arch(void);
+
+#endif
--- init/main.c
+++ init/main.c
@@ -1,6 +1,12 @@
 #include <printk.h>
+#include <irqflags.h>
+#include <init.h>
 
 void start_kernel(void)
 {
+	local_irq_disable();	
+	setup_arch();
+
+
 	printk("[riscv64]: hello kernel!\n");
 }
