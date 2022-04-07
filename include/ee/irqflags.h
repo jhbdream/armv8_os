d9ed933c045f2e4336e594cf81a20282ba28ff1b
jihongbin <longma@orbbec.com>
2022-04-07 19:53:51 +0800

    [update]: 添加aarch64兼容

--- include/ee/irqflags.h
+++ include/ee/irqflags.h
@@ -5,7 +5,7 @@
 
 #define local_irq_enable()  do { arch_local_irq_enable(); } while (0)
 #define local_irq_disable() do { arch_local_irq_disable(); } while (0)
-#define local_irq_save(flags)   do { arch_local_irq_save(flags); } while (0)
+#define local_irq_save(flags)   do { flags = arch_local_irq_save(); } while (0)
 #define local_irq_restore(flags) do { arch_local_irq_restore(flags); } while (0)
 
 #endif
