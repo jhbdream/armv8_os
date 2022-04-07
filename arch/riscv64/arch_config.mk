e363d7a9217534c36ed2387d5468d30a54ebab5f
jihongbin <longma@orbbec.com>
2022-04-07 17:35:57 +0800

    [update]: 修改编译参数 与eda对齐

--- arch/riscv64/arch_config.mk
+++ arch/riscv64/arch_config.mk
@@ -1,4 +1,4 @@
 CPPFLAGS += 
-CFLAGS += -march=rv64g -nostdinc -fno-stack-protector 
-ASFLAGS += 
-LDFLAGS += -Wl,--build-id=none -fno-stack-protector -static -nostartfiles -nostdlib -ffreestanding -fno-common
+CFLAGS += -march=rv64im -mabi=lp64 -nostdinc -fno-stack-protector 
+ASFLAGS += -march=rv64im -mabi=lp64 -nostdinc -fno-stack-protector 
+LDFLAGS += -Wl,--build-id=none -march=rv64im -mabi=lp64 -fno-stack-protector -static -nostartfiles -nostdlib -ffreestanding -fno-common
