3d63b1e90dbd52ead08ec697d522b365f334c42c
jihongbin <jhb_ee@163.com>
2022-05-15 23:16:39 +0800

    [header]: add some macro define
    Signed-off-by: jihongbin <jhb_ee@163.com>

--- include/compiler_attribute.h
+++ include/compiler_attribute.h
@@ -2,5 +2,7 @@
 #define __LINUX_COMPILER_ATTRIBUTES_H
 
 #define __attribute_const__             __attribute__((__const__))
+#define __aligned(x)                    __attribute__((__aligned__(x)))
+#define __section(section)              __attribute__((__section__(section)))
 
 #endif
