4a4e765f1185c0279961ccefce9a4c00b9ee3204
jihongbin <longma@orbbec.com>
2022-07-15 16:45:19 +0800

    [riscv][setup]: 整理初始化顺序
    Signed-off-by: jihongbin <longma@orbbec.com>

--- arch/riscv64/boot/setup.c
+++ arch/riscv64/boot/setup.c
@@ -1,3 +1,4 @@
+#include "driver/console.h"
 #include <ee/irqflags.h>
 
 void setup_arch(void)
@@ -5,6 +6,8 @@ void setup_arch(void)
 	extern void riscv_plic_init(void);
 	riscv_plic_init();
 	local_irq_enable();
+
+	console_init();
 
 	extern void riscv_timer_init(void);
 	riscv_timer_init();
--- init/main.c
+++ init/main.c
@@ -4,13 +4,14 @@
 #include <config.h>
 #include <ee/irqflags.h>
 #include <ee/init.h>
+#include <io.h>
+#include <driver/interrupt.h>
 
 void start_kernel(void)
 {
-    console_init();
-    setup_arch();
+   setup_arch();
 
-	printk("hello kernel!\n");
+   printk("hello kernel!\n");
 
-   	while (1);
+   for(;;);
 }
