16541b061380770d861d252d0a83f12982a80fae
jihongbin <longma@orbbec.com>
2022-07-15 10:55:19 +0800

    [riscv][exception]: fix args [riscv][timer]: add timecmp init
    Signed-off-by: jihongbin <longma@orbbec.com>

--- driver/timer/riscv_timer.c
+++ driver/timer/riscv_timer.c
@@ -1,5 +1,10 @@
 #include <eeos.h>
 #include <asm/csr.h>
+#include <io.h>
+
+#define PLIC_BASE 0x60000000
+#define CLINT_OFFSET 0x04000000
+#define TIME_CMP_OFFSET 0x4000
 
 static u64 get_ticks(void)
 {
@@ -11,6 +16,16 @@ static u64 get_ticks(void)
 
 void riscv_timer_start(void)
 {
+	u64 val = -1ULL;
+	u32 mask = -1U;
+	void *addr;
+
+	/* init compare val */
+	addr = (void *)(PLIC_BASE + CLINT_OFFSET + TIME_CMP_OFFSET);
+	writel(val & mask, addr);
+	writel(val >> 32, addr + 4);
+
+	/* enable timer irq */
 	csr_set(mie, MIE_MTIE);
 }
 
