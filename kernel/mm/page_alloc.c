dc88d2c24ab31cbb19ae6f3c643999e06c524147
jihongbin <jhb_ee@163.com>
2022-07-19 00:18:35 +0800

    [page][mm]: add page size cal
    Signed-off-by: jihongbin <jhb_ee@163.com>

--- kernel/mm/page_alloc.c
+++ kernel/mm/page_alloc.c
@@ -1,5 +1,7 @@
+#include "printk.h"
 #include <mm/page_alloc.h>
 #include <ee/pgtable.h>
+#include <round.h>
 
 /* use buddy page alloc */
 
@@ -8,8 +10,10 @@ static struct page *page_base;
 int buddy_page_init(unsigned long mem_start, unsigned long mem_end)
 {
     unsigned long size, align;
-	unsigned long page_count = (mem_end - mem_start) >> PAGE_SHIFT;
+	unsigned long page_count = DIV_ROUND_UP((mem_end - mem_start), PAGE_SIZE);
+	size = sizeof(struct page) * page_count;
+	printk("total page count: [0x%x]\n", page_count);
+	printk("total page size: [0x%x]\n", size);
 
-	//size = sizeof(struct page) * 
 	return 0;
 }
