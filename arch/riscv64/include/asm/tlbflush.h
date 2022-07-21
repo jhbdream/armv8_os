55e5ba5dae5cc4af9ba529ef8f9feca6bab239e0
jihongbin <longma@orbbec.com>
2022-07-21 14:56:35 +0800

    [riscv]: add tlbflush ops
    Signed-off-by: jihongbin <longma@orbbec.com>

---
+++ arch/riscv64/include/asm/tlbflush.h
@@ -0,0 +1,15 @@
+#ifndef _ASM_RISCV_TLBFLUSH_H
+#define _ASM_RISCV_TLBFLUSH_H
+
+static inline void local_flush_tlb_all(void)
+{
+	__asm__ __volatile__ ("sfence.vma" : : : "memory");
+}
+
+/* Flush one page from local TLB */
+static inline void local_flush_tlb_page(unsigned long addr)
+{
+	__asm__ __volatile__ ("sfence.vma %0" : : "r" (addr) : "memory");
+}
+
+#endif
