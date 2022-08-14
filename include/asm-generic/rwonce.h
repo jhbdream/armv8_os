48fdbddb9295521b6867b232ff4f1cb352fb32ad
jihongbin <jhb_ee@163.com>
2022-08-14 06:05:18 +0000

    add simpel rwonce
    Signed-off-by: jihongbin <jhb_ee@163.com>

---
+++ include/asm-generic/rwonce.h
@@ -0,0 +1,23 @@
+#ifndef __RWONCE_H__
+#define __RWONCE_H__
+
+#ifndef __READ_ONCE
+#define __READ_ONCE(x)  (*(const volatile typeof(x) *)&(x))
+#endif
+
+#define READ_ONCE(x)                            \
+({                                  \
+    __READ_ONCE(x);                         \
+})
+
+#define __WRITE_ONCE(x, val)                        \
+do {                                    \
+    *(volatile typeof(x) *)&(x) = (val);                \
+} while (0)
+
+#define WRITE_ONCE(x, val)                      \
+do {                                    \
+    __WRITE_ONCE(x, val);                       \
+} while (0)
+
+#endif
