c345f0251c358377b42eb0597848be62728eafc5
jihongbin <jhb_ee@163.com>
2023-04-05 16:13:08 +0000

    [mmu v2]: update
    Signed-off-by: jihongbin <jhb_ee@163.com>

--- arch/aarch64/mmu_v2/mmu.c
+++ arch/aarch64/mmu_v2/mmu.c
@@ -44,48 +44,128 @@ static inline void set_pte(pte_t *ptep, pte_t pte)
 	*ptep = pte;
 }
 
-static void create_pte_mapping(pte_t *pte, unsigned long phys,
-			       unsigned long virt, unsigned long size,
-			       pgprot_t prot,
-			       unsigned long (*pgtable_alloc)(int))
+static inline void set_pmd(pmd_t *pmdp, pmd_t pmd)
+{
+	*pmdp = pmd;
+}
+
+static inline void set_pud(pud_t *pudp, pud_t pud)
+{
+	*pudp = pud;
+}
+
+static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
+{
+	*pgdp = pgd;
+}
+
+/**
+ * 填充pte表项
+ *
+ * 1. size PMD表大小范围 2M
+ * 2. virt + size 不能跨越PMD边界 2M
+ * 3. 否则连续写pte会溢出
+ * 4. pte 是物理地址，在使用之前需要借助fixmap机制转换，使用完成之后释放
+ */
+static void create_pte_mapping(pte_t *pte, unsigned long addr,
+			       unsigned long end, unsigned long phys,
+			       pgprot_t prot)
 {
 	BUG_ON(pte == NULL);
-	BUG_ON(pgtable_alloc == NULL);
 	BUG_ON(pgprot_val(prot) == 0);
 
 	pte_t *ptep;
-	unsigned long addr, end, next;
 
-	ptep = pte + PTE_INDEX(virt);
-
-	phys = phys & PAGE_MASK;
-	addr = virt & PAGE_MASK;
-	end = addr + size;
+	ptep = pte + PTE_INDEX(addr);
 
 	do {
-		next = addr + PAGE_SIZE;
+		// 填充PTE页表项
+		set_pte(ptep, _pte((phys | pgprot_val(prot))));
+		phys = phys + PAGE_SIZE;
+
+		/* 使用虚拟地址判是因为要保证虚拟地址不跨越边界 */
+		addr = addr + PAGE_SIZE;
+		if (addr >= end)
+			break;
+
+	} while (ptep++);
+}
+
+static void create_pmd_mapping(pmd_t *pmd, unsigned long addr,
+			       unsigned long end, unsigned long phys,
+			       pgprot_t prot,
+			       unsigned long (*pgtable_alloc)(int))
+{
+	BUG_ON(pmd == NULL);
+	BUG_ON(pgtable_alloc == NULL);
+	BUG_ON(pgprot_val(prot) == 0);
+
+	pte_t *pte;
+	pmd_t *pmdp;
+	unsigned long next;
+
+	pmdp = pmd + PMD_INDEX(addr);
+
+	do {
+		pte = (pte_t *)pmd_val(*pmdp);
+		if (pte == NULL) {
+			// 下一级页表为空，需要分配一个页表并填充到pmdp中
+			// 返回的是物理地址
+			unsigned long page = pgtable_alloc(1);
+
+			// 填充下一级页表物理地址到当前表项
+			// 同时需要补充其他位域的内容
+			set_pmd(pmdp, _pmd(page));
+
+			pte = (pte_t *)page;
+		}
+
+		/* 单次映射不能超过一个PMD大小 */
+		next = (addr + PMD_SIZE) & PMD_MASK;
 		if (next > end)
 			next = end;
 
-		// 填充PTE页表项
+		create_pte_mapping(pte, addr, next, phys, prot);
 
-		set_pte(ptep, _pte(addr | pgprot_val(prot)));
+		phys += next - addr;
 
-	} while (ptep++, addr = next, addr != end);
+		addr = next;
+		if (addr >= end)
+			break;
+
+	} while (pmdp++);
 }
 
-static void create_pmd_mapping(pmd_t *pmd, unsigned long phys,
-			       unsigned long virt, unsigned long size,
+static void create_pud_mapping(pud_t *pud, unsigned long addr,
+			       unsigned long end, unsigned long phys,
 			       pgprot_t prot,
 			       unsigned long (*pgtable_alloc)(int))
 {
-}
+	BUG_ON(pud == NULL);
+	BUG_ON(pgtable_alloc == NULL);
+	BUG_ON(pgprot_val(prot) == 0);
 
-static void create_pud_mapping(pud_t *pud, unsigned long phys,
-			       unsigned long virt, unsigned long size,
-			       pgprot_t prot,
-			       unsigned long (*pgtable_alloc)(int))
-{
+	pud_t *pudp;
+	unsigned long next;
+
+	pudp = pud + PUD_INDEX(addr);
+
+	do {
+		/* 单次映射不能超过一个PUD大小 */
+		next = (addr + PUD_SIZE) & PUD_MASK;
+		if (next > end)
+			next = end;
+
+		create_pmd_mapping((pmd_t *)pud_val(*pudp), addr, next, phys,
+				   prot, pgtable_alloc);
+
+		phys += next - addr;
+
+		addr = next;
+		if (addr >= end)
+			break;
+
+	} while (pudp++);
 }
 
 /**
