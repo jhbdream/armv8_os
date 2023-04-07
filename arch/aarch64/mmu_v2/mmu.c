4f6561764741cc60007890b3c5ba2bbdc3d58dee
Hongbin Ji <jhb_ee@163.com>
2023-04-07 21:31:14 +0800

    [mmu_v2]: add base code
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- arch/aarch64/mmu_v2/mmu.c
+++ arch/aarch64/mmu_v2/mmu.c
@@ -10,7 +10,9 @@
  * 1. 考虑与 vmap 共用接口
  * 2. 实现 fix_map 机制, 用于 page_alloc 实现之前使用
  * 3. 实现恒等映射，定义静态恒等映射页表
- *
+ * 4. 使用统一的页表分配接口时，如何在内存分配接口生效之前分配内存
+ * 5. 早期虚拟地址物理地址转换接口？
+ * 6. 中期虚拟地址物理地址转换接口?
  */
 
 /* config for 48bit va + 4 level page table */
@@ -39,6 +41,18 @@
 #define PMD_INDEX(va) (((va) >> PMD_SHIFT) & (PTRS_PER_PMD - 1))
 #define PTE_INDEX(va) (((va) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1))
 
+#define PAGE_KERNEL_EXEC _pgprot(0)
+
+// 静态定义 pgd 页表空间
+// 1. 创建内核代码映射
+// 2. 创建fixmap临时访问映射，用于访问内核代码之外物理空间
+// 3. 如果映射到四级，只能映射2M空间，故建议只映射三级，最小映射单位是2M，映射范围是1G
+
+pgd_t init_pgd[PTRS_PER_PGD];
+pud_t init_pud[PTRS_PER_PUD];
+pmd_t init_pmd[PTRS_PER_PMD];
+pmd_t init_pte[PTRS_PER_PTE];
+
 static inline void set_pte(pte_t *ptep, pte_t pte)
 {
 	*ptep = pte;
@@ -60,30 +74,75 @@ static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
 }
 
 /**
+ *  早期申请页表内存时使用
+ */
+typedef enum {
+	NORMAL_PAGE = 0,
+	PGD_PAGE,
+	PUD_PAGE,
+	PMD_PAGE,
+	PTE_PAGE,
+} pgtable_alloc_type_e;
+
+/**
+ * @brief 用于早期分配页表，分配静态定义的页表
+ *
+ * @param type
+ *
+ * @return 分配页表的物理地址
+ */
+static unsigned long early_page_alloc(int type)
+{
+	switch (type) {
+	case PGD_PAGE:
+		return (unsigned long)init_pgd;
+	case PUD_PAGE:
+		return (unsigned long)init_pud;
+	case PMD_PAGE:
+		return (unsigned long)init_pmd;
+	case PTE_PAGE:
+		return (unsigned long)init_pte;
+	default:
+		return (unsigned long)NULL;
+	}
+}
+
+unsigned long memblock_pgtable_alloc(int type)
+{
+	return (unsigned long)NULL;
+}
+
+unsigned long early_get_va(unsigned long pa)
+{
+	return pa;
+}
+
+/**
  * 填充pte表项
  *
  * 1. size PMD表大小范围 2M
  * 2. virt + size 不能跨越PMD边界 2M
  * 3. 否则连续写pte会溢出
- * 4. pte 是物理地址，在使用之前需要借助fixmap机制转换，使用完成之后释放
+ * xxxx pte 是物理地址，在使用之前需要借助fixmap机制转换，使用完成之后释放
+ *
+ * ptep 使用虚拟地址
+ * 表示pte页表指针
  */
-static void create_pte_mapping(pte_t *pte, unsigned long addr,
+static void create_pte_mapping(pte_t *ptep, unsigned long addr,
 			       unsigned long end, unsigned long phys,
 			       pgprot_t prot)
 {
-	BUG_ON(pte == NULL);
+	BUG_ON(ptep == NULL);
 	BUG_ON(pgprot_val(prot) == 0);
 
-	pte_t *ptep;
-
-	ptep = pte + PTE_INDEX(addr);
+	pte_t *pte;
+	pte = ptep + PTE_INDEX(addr);
 
 	do {
 		// 填充PTE页表项
-		set_pte(ptep, _pte((phys | pgprot_val(prot))));
-		phys = phys + PAGE_SIZE;
+		set_pte(pte, _pte((phys | pgprot_val(prot))));
 
-		/* 使用虚拟地址判是因为要保证虚拟地址不跨越边界 */
+		phys = phys + PAGE_SIZE;
 		addr = addr + PAGE_SIZE;
 		if (addr >= end)
 			break;
@@ -91,41 +150,43 @@ static void create_pte_mapping(pte_t *pte, unsigned long addr,
 	} while (ptep++);
 }
 
-static void create_pmd_mapping(pmd_t *pmd, unsigned long addr,
+static void create_pmd_mapping(pmd_t *pmdp, unsigned long addr,
 			       unsigned long end, unsigned long phys,
 			       pgprot_t prot,
-			       unsigned long (*pgtable_alloc)(int))
+			       unsigned long (*pgtable_alloc)(int),
+			       unsigned long (*pgtable_get_va)(unsigned long))
 {
-	BUG_ON(pmd == NULL);
-	BUG_ON(pgtable_alloc == NULL);
+	BUG_ON(pmdp == NULL);
 	BUG_ON(pgprot_val(prot) == 0);
 
-	pte_t *pte;
-	pmd_t *pmdp;
+	pmd_t *pmd;
+	pte_t *ptep;
 	unsigned long next;
+	unsigned long pte_page;
 
-	pmdp = pmd + PMD_INDEX(addr);
+	pmd = pmdp + PMD_INDEX(addr);
 
 	do {
-		pte = (pte_t *)pmd_val(*pmdp);
-		if (pte == NULL) {
+		pte_page = pmd_val(*pmd);
+		if (!pte_page) {
 			// 下一级页表为空，需要分配一个页表并填充到pmdp中
 			// 返回的是物理地址
-			unsigned long page = pgtable_alloc(1);
+			pte_page = pgtable_alloc(PTE_PAGE);
 
 			// 填充下一级页表物理地址到当前表项
 			// 同时需要补充其他位域的内容
-			set_pmd(pmdp, _pmd(page));
-
-			pte = (pte_t *)page;
+			set_pmd(pmdp, _pmd(pte_page | 0));
 		}
+
+		// 将pte的物理地址转换为虚拟地址
+		ptep = (pte_t *)pgtable_get_va(pte_page);
 
 		/* 单次映射不能超过一个PMD大小 */
 		next = (addr + PMD_SIZE) & PMD_MASK;
 		if (next > end)
 			next = end;
 
-		create_pte_mapping(pte, addr, next, phys, prot);
+		create_pte_mapping(ptep, addr, next, phys, prot);
 
 		phys += next - addr;
 
@@ -136,28 +197,44 @@ static void create_pmd_mapping(pmd_t *pmd, unsigned long addr,
 	} while (pmdp++);
 }
 
-static void create_pud_mapping(pud_t *pud, unsigned long addr,
+static void create_pud_mapping(pud_t *pudp, unsigned long addr,
 			       unsigned long end, unsigned long phys,
 			       pgprot_t prot,
-			       unsigned long (*pgtable_alloc)(int))
+			       unsigned long (*pgtable_alloc)(int),
+			       unsigned long (*pgtable_get_va)(unsigned long))
 {
-	BUG_ON(pud == NULL);
-	BUG_ON(pgtable_alloc == NULL);
+	BUG_ON(pudp == NULL);
 	BUG_ON(pgprot_val(prot) == 0);
 
-	pud_t *pudp;
+	pud_t *pud;
+	pmd_t *pmdp;
 	unsigned long next;
+	unsigned long pmd_page;
 
-	pudp = pud + PUD_INDEX(addr);
+	pud = pudp + PMD_INDEX(addr);
 
 	do {
-		/* 单次映射不能超过一个PUD大小 */
+		pmd_page = pud_val(*pud);
+		if (!pmd_page) {
+			// 下一级页表为空，需要分配一个页表并填充到pudp中
+			// 返回的是物理地址
+			pmd_page = pgtable_alloc(PMD_PAGE);
+
+			// 填充下一级页表物理地址到当前表项
+			// 同时需要补充其他位域的内容
+			set_pud(pudp, _pud(pmd_page | 0));
+		}
+
+		// 将pmd的物理地址转换为虚拟地址
+		pmdp = (pmd_t *)pgtable_get_va(pmd_page);
+
+		/* 单次映射不能超过一个PMD大小 */
 		next = (addr + PUD_SIZE) & PUD_MASK;
 		if (next > end)
 			next = end;
 
-		create_pmd_mapping((pmd_t *)pud_val(*pudp), addr, next, phys,
-				   prot, pgtable_alloc);
+		create_pmd_mapping(pmdp, addr, next, phys, prot, pgtable_alloc,
+				   pgtable_get_va);
 
 		phys += next - addr;
 
@@ -182,28 +259,85 @@ static void create_pud_mapping(pud_t *pud, unsigned long addr,
  * 1. 根据虚拟地址找到 pud 表项
  *
  */
-static void create_pgd_mapping(pgd_t *pgdir, unsigned long phys,
+static void create_pgd_mapping(pgd_t *pgdp, unsigned long phys,
 			       unsigned long virt, unsigned long size,
 			       pgprot_t prot,
-			       unsigned long (*pgtable_alloc)(int))
+			       unsigned long (*pgtable_alloc)(int),
+			       unsigned long (*pgtable_get_va)(unsigned long))
 {
-	BUG_ON(pgdir == NULL);
-	BUG_ON(pgtable_alloc == NULL);
+	BUG_ON(pgdp == NULL);
 	BUG_ON(pgprot_val(prot) == 0);
 
-	pgd_t *pgdp;
-	unsigned long addr, end, next;
-
-	pgdp = pgdir + PGD_INDEX(virt);
+	pgd_t *pgd;
+	pud_t *pudp;
+	unsigned long next;
+	unsigned long pud_page;
 
 	phys = phys & PAGE_MASK;
-	addr = virt & PAGE_MASK;
-	end = addr + size;
+	unsigned long addr = virt & PAGE_MASK;
+	unsigned int end = addr + size;
+
+	pgd = pgdp + PMD_INDEX(addr);
 
 	do {
-		next = addr + PGDIR_SIZE;
+		pud_page = pgd_val(*pgd);
+		if (!pud_page) {
+			// 下一级页表为空，需要分配一个页表并填充到pgdp中
+			// 返回的是物理地址
+			pud_page = pgtable_alloc(PUD_PAGE);
+
+			// 填充下一级页表物理地址到当前表项
+			// 同时需要补充其他位域的内容
+			set_pgd(pgdp, _pgd(pud_page | 0));
+		}
+
+		// 将pgd的物理地址转换为虚拟地址
+		pudp = (pud_t *)pgtable_get_va(pud_page);
+
+		/* 单次映射不能超过一个PMD大小 */
+		next = (addr + PUD_SIZE) & PUD_MASK;
 		if (next > end)
 			next = end;
 
-	} while (pgdp++, addr = next, addr != end);
+		create_pud_mapping(pudp, addr, next, phys, prot, pgtable_alloc,
+				   pgtable_get_va);
+
+		phys += next - addr;
+
+		addr = next;
+		if (addr >= end)
+			break;
+
+	} while (pgdp++);
+}
+
+void create_kernel_map(void)
+{
+	/**
+     *  1. kernel va
+	 *  2. kernel pa
+	 *  3. kernel size (align to 2M)
+	 *  4. kernel prot
+	 *
+	 *  - map 3 level page_table
+	 *  - only use init page_table
+     */
+
+	/**
+	 * create kernel image map in early_pd_dir
+	 *
+	 */
+	phys_addr_t pa;
+	unsigned long va;
+	unsigned long size;
+
+	extern unsigned long __kimage_start[], __kimage_end[];
+	pa = (unsigned long)__kimage_start;
+	va = (unsigned long)__kimage_start;
+	size = (unsigned long)__kimage_end - (unsigned long)__kimage_start;
+
+	BUG_ON((pa % PMD_SIZE) != 0);
+
+	create_pgd_mapping(init_pgd, pa, va, size, PAGE_KERNEL_EXEC,
+			   early_page_alloc, early_get_va);
 }
