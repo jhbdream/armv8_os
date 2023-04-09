39b235f649fbbe3275ad70945d83585ece0f0c2f
Hongbin Ji <jhb_ee@163.com>
2023-04-09 16:33:36 +0800

    [linkage.h]: remove unused define && files
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- include/linkage.h
+++ include/linkage.h
@@ -1,12 +1,7 @@
-#ifndef __LINK_AGE_H
-#define __LINK_AGE_H
+#ifndef __LINKAGE_H
+#define __LINKAGE_H
 
 #include <compiler_attribute.h>
-#include <asm/linkage.h>
-
-#define __page_aligned_data                                                    \
-	__section(".data..page_aligned") __aligned(PAGE_SIZE)
-#define __page_aligned_bss __section(".bss..page_aligned") __aligned(PAGE_SIZE)
 
 /* Some toolchains use other characters (e.g. '`') to mark new line in macro */
 #ifndef ASM_NL
@@ -14,11 +9,6 @@
 #endif
 
 #ifdef __ASSEMBLY__
-
-#ifndef __ALIGN
-#define __ALIGN .align 4, 0x90
-#define __ALIGN_STR ".align 4,0x90"
-#endif
 
 #ifndef LINKER_SCRIPT
 #define ALIGN __ALIGN
