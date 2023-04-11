3a3a0275c8f54ab1056f45cece38056dc3c98405
jihongbin <jhb_ee@163.com>
2023-04-11 16:06:48 +0000

    [arm64]: 整理清理代码
    Signed-off-by: jihongbin <jhb_ee@163.com>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -2,6 +2,9 @@
  * @file memory.h
  * @author jihongbin (longma@orbbec.com)
  * @brief
+ *	1. 定义虚拟地址空间划分
+ *  2. 每个架构私有
+ *  3. 包含 PAGE_OFFSET \ FIXADDR_TOP ...
  * @version 0.1
  * @date 2022-05-07
  * @copyright Copyright (c) {2015-2021} Orbbec
@@ -11,71 +14,23 @@
 #ifndef __ASM_MEMORY_H
 #define __ASM_MEMORY_H
 
-#include <config.h>
 #include <ee/sizes.h>
+#include <config.h>
 #include <type.h>
 
-#define VA_BITS (CONFIG_ARM64_VA_BITS)
-#define _PAGE_OFFSET(va) (-(UL(1) << (va)))
-#define PHYS_OFFSET (memory_start)
+#define VA_BITS (48)
+
+/* 代码链接地址 */
+#define KIMAGE_VADDR (0xFFFF000000080000)
+
+/* FIXMAP 起始地址 */
+#define FIXADDR_SIZE (SZ_512M)
+#define FIXADDR_TOP (KIMAGE_VADDR + FIXADDR_SIZE)
+
+/* 线性映射起始地址? */
 #define PAGE_OFFSET (0xFFFF800000000000)
 
-/* 早期的恒等映射只做了 1G PUD 映射，所以代码段的虚拟地址与实际加载地址在1G的偏移要相同 */
-/* 因为线性映射虚拟地址物理地址转换逻辑 PAGE_OFFSET 需要大于 KIMAGE_VADDR */
-#define KIMAGE_VADDR (0xFFFF000000080000)
-#define FIXADDR_TOP (KIMAGE_VADDR + SZ_512M)
-
+/* VMALLOC 虚拟地址 */
 #define VMALLOC_START 0xFFFF900000000000
 
-/*
- * Memory types available.
- *
- * IMPORTANT: MT_NORMAL must be index 0 since vm_get_page_prot() may 'or' in
- *        the MT_NORMAL_TAGGED memory type for PROT_MTE mappings. Note
- *        that protection_map[] only contains MT_NORMAL attributes.
- */
-#define MT_NORMAL 0
-#define MT_NORMAL_TAGGED 1
-#define MT_NORMAL_NC 2
-#define MT_DEVICE_nGnRnE 3
-#define MT_DEVICE_nGnRE 4
-
-#ifndef __ASSEMBLY__
-
-extern s64 memory_start;
-extern uint64_t kimage_voffset;
-
-#define PHYS_PFN_OFFSET (PHYS_OFFSET >> PAGE_SHIFT)
-#define ARCH_PFN_OFFSET ((unsigned long)PHYS_PFN_OFFSET)
-
-#define __lm_to_phys(addr) (((addr)-PAGE_OFFSET) + PHYS_OFFSET)
-#define __kimg_to_phys(addr) ((addr)-kimage_voffset)
-#define __is_lm_address(addr) (((u64)(addr) >= PAGE_OFFSET))
-
-#define __phys_to_virt(x) ((x - PHYS_OFFSET) | PAGE_OFFSET)
-#define __virt_to_phys(x)                                                      \
-	(__is_lm_address(x) ? __lm_to_phys(x) : __kimg_to_phys(x))
-
-#define virt_to_phys virt_to_phys
-static inline phys_addr_t virt_to_phys(const volatile void *x)
-{
-	return (__virt_to_phys((unsigned long)(x)));
-}
-
-#define phys_to_virt phys_to_virt
-static inline void *phys_to_virt(phys_addr_t x)
-{
-	return (void *)(__phys_to_virt(x));
-}
-
-#define __phys_to_kimg(x) ((unsigned long)((x) + kimage_voffset))
-#define __kimg_to_phys(addr) ((addr)-kimage_voffset)
-#define __phys_addr_symbol(x) __kimg_to_phys((phys_addr_t)(x))
-
-#define __pa(x) virt_to_phys(x)
-#define __pa_symbol(x) __phys_addr_symbol(x)
-
-#define __va(x) phys_to_virt(x)
-
-#endif /* #ifndef __ASSEMBLY__ */
 #endif
--- arch/arm64/mmu_v2/aarch64_mmu.h
+++
@@ -1,6 +0,0 @@
-#ifndef __AARCH64_MMU_H__
-#define __AARCH64_MMU_H__
-
-
-
-#endif
