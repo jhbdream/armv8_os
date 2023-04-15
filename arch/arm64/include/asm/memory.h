1701ecbdbc3a855c44e4cb1b6256bd345ef9fba1
Hongbin Ji <jhb_ee@163.com>
2023-04-15 20:58:34 +0800

    update format
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -2,13 +2,19 @@
  * @file memory.h
  * @author jihongbin (longma@orbbec.com)
  * @brief
- *	1. 定义虚拟地址空间划分
- *  2. 每个架构私有
- *  3. 包含 PAGE_OFFSET \ FIXADDR_TOP ...
+ *
  * @version 0.1
  * @date 2022-05-07
  * @copyright Copyright (c) {2015-2021} Orbbec
  *
+ */
+
+/**
+ *
+ *
+ *	1. 定义虚拟地址空间划分
+ *  2. 每个架构私有
+ *  3. 包含 PAGE_OFFSET \ FIXADDR_TOP ...
  */
 
 #ifndef __ASM_MEMORY_H
@@ -18,17 +24,17 @@
 #include <config.h>
 #include <type.h>
 
-#define VA_BITS (48)
+#define VA_BITS       (48)
 
 /* 代码链接地址 */
-#define KIMAGE_VADDR (0xFFFF000000080000)
+#define KIMAGE_VADDR  (0xFFFF000000080000)
 
 /* FIXMAP 起始地址 */
-#define FIXADDR_SIZE (SZ_512M)
-#define FIXADDR_TOP (KIMAGE_VADDR + FIXADDR_SIZE)
+#define FIXADDR_SIZE  (SZ_512M)
+#define FIXADDR_TOP   (KIMAGE_VADDR + FIXADDR_SIZE)
 
 /* 线性映射起始地址? */
-#define PAGE_OFFSET (0xFFFF800000000000)
+#define PAGE_OFFSET   (0xFFFF800000000000)
 
 /* VMALLOC 虚拟地址 */
 #define VMALLOC_START 0xFFFF900000000000
