1816652066389a63efc7e6d824e40ccef452cb05
Hongbin Ji <jhb_ee@163.com>
2025-03-18 20:53:03 +0800

    [include]: remove some unued code
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -20,23 +20,76 @@
 #ifndef __ASM_MEMORY_H
 #define __ASM_MEMORY_H
 
-#include <ee/sizes.h>
 #include <config.h>
 #include <type.h>
 
-#define VA_BITS       (48)
+#include <ee/sizes.h>
+
+#define VA_BITS          (48)
+
+/* 物理内存基地址 */
+#define PHYS_OFFSET      (0x40000000)
 
 /* 代码链接地址 */
-#define KIMAGE_VADDR  (0xFFFF000000080000)
+#define KIMAGE_VADDR     (0xFFFF000000080000)
 
-/* FIXMAP 起始地址 */
-#define FIXADDR_SIZE  (SZ_512M)
-#define FIXADDR_TOP   (KIMAGE_VADDR + FIXADDR_SIZE)
-
-/* 线性映射起始地址? */
-#define PAGE_OFFSET   (0xFFFF800000000000)
+/* 线性映射起始地址 */
+#define PAGE_OFFSET      (0xFFFF000100000000)
+#define PAGE_END         (0xFFFF0001FFFFFFFF)
 
 /* VMALLOC 虚拟地址 */
-#define VMALLOC_START 0xFFFF900000000000
+#define VMALLOC_START    (0xFFFF000200000000)
+#define VMALLOC_END      (0xFFFF0002FFFFFFFF)
+
+/*
+ * Memory types available.
+ *
+ * IMPORTANT: MT_NORMAL must be index 0 since vm_get_page_prot() may 'or' in
+ *            the MT_NORMAL_TAGGED memory type for PROT_MTE mappings. Note
+ *            that protection_map[] only contains MT_NORMAL attributes.
+ */
+#define MT_NORMAL        0
+#define MT_NORMAL_TAGGED 1
+#define MT_NORMAL_NC     2
+#define MT_DEVICE_nGnRnE 3
+#define MT_DEVICE_nGnRE  4
+
+#ifndef __ASSEMBLY__
+
+extern uint64_t kimage_voffset;
+
+/*
+ * Check whether an arbitrary address is within the linear map, which
+ * lives in the [PAGE_OFFSET, PAGE_END) interval at the bottom of the
+ * kernel's TTBR1 address range.
+ */
+#define __is_lm_address(addr)     (((u64)(addr)-PAGE_OFFSET) < (PAGE_END - PAGE_OFFSET))
+
+#define __lm_to_phys(addr)        (((addr)-PAGE_OFFSET) + PHYS_OFFSET)
+#define __kimg_to_phys(addr)      ((addr)-kimage_voffset)
+
+#define __virt_to_phys_nodebug(x) ({ __is_lm_address(x) ? __lm_to_phys(x) : __kimg_to_phys(x); })
+
+#define __pa_symbol_nodebug(x)    __kimg_to_phys((phys_addr_t)(x))
+
+#define __virt_to_phys(x)         __virt_to_phys_nodebug(x)
+#define __phys_addr_symbol(x)     __pa_symbol_nodebug(x)
+
+#define __phys_to_virt(x)         ((unsigned long)((x)-PHYS_OFFSET) | PAGE_OFFSET)
+#define __phys_to_kimg(x)         ((unsigned long)((x) + kimage_voffset))
+
+#define virt_to_phys              virt_to_phys
+static inline phys_addr_t virt_to_phys(const volatile void *x)
+{
+	return __virt_to_phys((unsigned long)(x));
+}
+
+#define phys_to_virt phys_to_virt
+static inline void *phys_to_virt(phys_addr_t x)
+{
+	return (void *)(__phys_to_virt(x));
+}
+
+#endif
 
 #endif
