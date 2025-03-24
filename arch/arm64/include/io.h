ab2847d51f22aaa5d54e3998b062f5aee7651f5f
Hongbin Ji <jhb_ee@163.com>
2025-03-24 15:47:59 +0800

    [arm64]: update io code
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- arch/arm64/include/io.h
+++ arch/arm64/include/io.h
@@ -10,31 +10,31 @@
  * Generic IO read/write.  These perform native-endian accesses.
  */
 #define __raw_writeb __raw_writeb
-static inline void __raw_writeb(u8 val, volatile void __iomem *addr)
+static inline void __raw_writeb(u8 val, volatile void *addr)
 {
 	asm volatile("strb %w0, [%1]" : : "rZ"(val), "r"(addr));
 }
 
 #define __raw_writew __raw_writew
-static inline void __raw_writew(u16 val, volatile void __iomem *addr)
+static inline void __raw_writew(u16 val, volatile void *addr)
 {
 	asm volatile("strh %w0, [%1]" : : "rZ"(val), "r"(addr));
 }
 
 #define __raw_writel __raw_writel
-static __always_inline void __raw_writel(u32 val, volatile void __iomem *addr)
+static __always_inline void __raw_writel(u32 val, volatile void *addr)
 {
 	asm volatile("str %w0, [%1]" : : "rZ"(val), "r"(addr));
 }
 
 #define __raw_writeq __raw_writeq
-static inline void __raw_writeq(u64 val, volatile void __iomem *addr)
+static inline void __raw_writeq(u64 val, volatile void *addr)
 {
 	asm volatile("str %x0, [%1]" : : "rZ"(val), "r"(addr));
 }
 
 #define __raw_readb __raw_readb
-static inline u8 __raw_readb(const volatile void __iomem *addr)
+static inline u8 __raw_readb(const volatile void *addr)
 {
 	u8 val;
 	asm volatile("ldrb %w0, [%1]" : "=r"(val) : "r"(addr));
@@ -42,7 +42,7 @@ static inline u8 __raw_readb(const volatile void __iomem *addr)
 }
 
 #define __raw_readw __raw_readw
-static inline u16 __raw_readw(const volatile void __iomem *addr)
+static inline u16 __raw_readw(const volatile void *addr)
 {
 	u16 val;
 
@@ -51,7 +51,7 @@ static inline u16 __raw_readw(const volatile void __iomem *addr)
 }
 
 #define __raw_readl __raw_readl
-static __always_inline u32 __raw_readl(const volatile void __iomem *addr)
+static __always_inline u32 __raw_readl(const volatile void *addr)
 {
 	u32 val;
 	asm volatile("ldr %w0, [%1]" : "=r"(val) : "r"(addr));
@@ -59,7 +59,7 @@ static __always_inline u32 __raw_readl(const volatile void __iomem *addr)
 }
 
 #define __raw_readq __raw_readq
-static inline u64 __raw_readq(const volatile void __iomem *addr)
+static inline u64 __raw_readq(const volatile void *addr)
 {
 	u64 val;
 	asm volatile("ldr %0, [%1]" : "=r"(val) : "r"(addr));
@@ -68,52 +68,56 @@ static inline u64 __raw_readq(const volatile void __iomem *addr)
 
 /* IO barriers */
 /*								                                \
-* Create a dummy control dependency from the IO read to any	    \
-* later instructions. This ensures that a subsequent call to	\
-* udelay() will be ordered due to the ISB in get_cycles().	    \
-*/
+ * Create a dummy control dependency from the IO read to any	    \
+ * later instructions. This ensures that a subsequent call to	\
+ * udelay() will be ordered due to the ISB in get_cycles().	    \
+ */
 
-#define __iormb(v)                                                             \
-	({                                                                     \
-		unsigned long tmp;                                             \
-                                                                               \
-		dma_rmb();                                                     \
-                                                                               \
-		asm volatile("eor	%0, %1, %1\n"                                \
-			     "cbnz	%0, ."                                      \
-			     : "=r"(tmp)                                       \
-			     : "r"((unsigned long)(v))                         \
-			     : "memory");                                      \
+#define __iormb(v) \
+	({ \
+		unsigned long tmp; \
+\
+		dma_rmb(); \
+\
+		asm volatile("eor	%0, %1, %1\n" \
+			     "cbnz	%0, ." \
+			     : "=r"(tmp) \
+			     : "r"((unsigned long)(v)) \
+			     : "memory"); \
 	})
 
+#define isb()       asm volatile("isb" : : : "memory")
+#define dmb(opt)    asm volatile("dmb " #opt : : : "memory")
+#define dsb(opt)    asm volatile("dsb " #opt : : : "memory")
+
 #define __io_par(v) __iormb(v)
-#define __iowmb() dma_wmb()
-#define __iomb() dma_mb()
+#define __iowmb()   dmb(oshst)
+#define __iomb()    dmb(osh)
 
 /*
  * Relaxed I/O memory access primitives. These follow the Device memory
  * ordering rules but do not guarantee any ordering relative to Normal memory
  * accesses.
  */
-#define readb_relaxed(c)                                                       \
-	({                                                                     \
-		u8 __r = __raw_readb(c);                                       \
-		__r;                                                           \
+#define readb_relaxed(c) \
+	({ \
+		u8 __r = __raw_readb(c); \
+		__r; \
 	})
-#define readw_relaxed(c)                                                       \
-	({                                                                     \
-		u16 __r = __raw_readw(c);                                      \
-		__r;                                                           \
+#define readw_relaxed(c) \
+	({ \
+		u16 __r = __raw_readw(c); \
+		__r; \
 	})
-#define readl_relaxed(c)                                                       \
-	({                                                                     \
-		u32 __r = __raw_readl(c);                                      \
-		__r;                                                           \
+#define readl_relaxed(c) \
+	({ \
+		u32 __r = __raw_readl(c); \
+		__r; \
 	})
-#define readq_relaxed(c)                                                       \
-	({                                                                     \
-		u64 __r = __raw_readq(c);                                      \
-		__r;                                                           \
+#define readq_relaxed(c) \
+	({ \
+		u64 __r = __raw_readq(c); \
+		__r; \
 	})
 
 #define writeb_relaxed(v, c) ((void)__raw_writeb((v), (c)))
@@ -126,50 +130,50 @@ static inline u64 __raw_readq(const volatile void __iomem *addr)
  * following Normal memory access. Writes are ordered relative to any prior
  * Normal memory access.
  */
-#define readb(c)                                                               \
-	({                                                                     \
-		u8 __v = readb_relaxed(c);                                     \
-		__iormb(__v);                                                  \
-		__v;                                                           \
+#define readb(c) \
+	({ \
+		u8 __v = readb_relaxed(c); \
+		__iormb(__v); \
+		__v; \
 	})
-#define readw(c)                                                               \
-	({                                                                     \
-		u16 __v = readw_relaxed(c);                                    \
-		__iormb(__v);                                                  \
-		__v;                                                           \
+#define readw(c) \
+	({ \
+		u16 __v = readw_relaxed(c); \
+		__iormb(__v); \
+		__v; \
 	})
-#define readl(c)                                                               \
-	({                                                                     \
-		u32 __v = readl_relaxed(c);                                    \
-		__iormb(__v);                                                  \
-		__v;                                                           \
+#define readl(c) \
+	({ \
+		u32 __v = readl_relaxed(c); \
+		__iormb(__v); \
+		__v; \
 	})
-#define readq(c)                                                               \
-	({                                                                     \
-		u64 __v = readq_relaxed(c);                                    \
-		__iormb(__v);                                                  \
-		__v;                                                           \
+#define readq(c) \
+	({ \
+		u64 __v = readq_relaxed(c); \
+		__iormb(__v); \
+		__v; \
 	})
 
-#define writeb(v, c)                                                           \
-	({                                                                     \
-		__iowmb();                                                     \
-		writeb_relaxed((v), (c));                                      \
+#define writeb(v, c) \
+	({ \
+		__iowmb(); \
+		writeb_relaxed((v), (c)); \
 	})
-#define writew(v, c)                                                           \
-	({                                                                     \
-		__iowmb();                                                     \
-		writew_relaxed((v), (c));                                      \
+#define writew(v, c) \
+	({ \
+		__iowmb(); \
+		writew_relaxed((v), (c)); \
 	})
-#define writel(v, c)                                                           \
-	({                                                                     \
-		__iowmb();                                                     \
-		writel_relaxed((v), (c));                                      \
+#define writel(v, c) \
+	({ \
+		__iowmb(); \
+		writel_relaxed((v), (c)); \
 	})
-#define writeq(v, c)                                                           \
-	({                                                                     \
-		__iowmb();                                                     \
-		writeq_relaxed((v), (c));                                      \
+#define writeq(v, c) \
+	({ \
+		__iowmb(); \
+		writeq_relaxed((v), (c)); \
 	})
 
 #endif /* __ASM_ARM_IO_H */
