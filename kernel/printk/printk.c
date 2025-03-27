0ddd56d4b38489702213b188a0d4b0d4cefad220
Hongbin Ji <jhb_ee@163.com>
2025-03-27 19:43:15 +0800

    [printk]: format code
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1,16 +1,18 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+
 #include <printk.h>
 #include <driver/console.h>
 
-struct console *uart_console;
+struct console *uart_console = NULL;
 
 int vprintk(const char *fmt, va_list args)
 {
 	char p_buf[1024];
-	int len;
+	int  len;
 
-	if (!uart_console) {
+	if (uart_console == NULL) {
 		return 0;
 	}
 
@@ -24,7 +26,7 @@ int vprintk(const char *fmt, va_list args)
 int printk(const char *fmt, ...)
 {
 	va_list args;
-	int r;
+	int     r;
 
 	va_start(args, fmt);
 	r = vprintk(fmt, args);
