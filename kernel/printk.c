b813c257239707e1c88f5b5b08a4426a1611e2aa
Hongbin Ji <jhb_ee@163.com>
2025-06-20 14:34:29 +0800

    [code]: clean up

--- kernel/printk.c
+++ kernel/printk.c
@@ -4,14 +4,34 @@
 #include <stdint.h>
 #include <string.h>
 
-#include <eeos/limits.h>
-#include <eeos/div64.h>
-
 #include <lib/ctype.h>
 
 #include <printk.h>
 
 #include <driver/console.h>
+
+/**
+ * do_div - returns 2 values: calculate remainder and update new dividend
+ * @n: uint64_t dividend (will be updated)
+ * @base: uint32_t divisor
+ *
+ * Summary:
+ * ``uint32_t remainder = n % base;``
+ * ``n = n / base;``
+ *
+ * Return: (uint32_t)remainder
+ *
+ * NOTE: macro parameter @n is evaluated multiple times,
+ * beware of side effects!
+ */
+#define do_div(n, base) \
+    ({ \
+        uint32_t __base = (base); \
+        uint32_t __rem; \
+        __rem = ((uint64_t)(n)) % __base; \
+        (n)   = ((uint64_t)(n)) / __base; \
+        __rem; \
+    })
 
 const char hex_asc[]       = "0123456789abcdef";
 const char hex_asc_upper[] = "0123456789ABCDEF";
@@ -547,7 +567,7 @@ qualifier:
  */
 int vsprintf(char *buf, const char *fmt, va_list args)
 {
-    return vsnprintf(buf, INT_MAX, fmt, args);
+    return vsnprintf(buf, 0xFFFFFFFF, fmt, args);
 }
 
 /**
@@ -560,13 +580,10 @@ int vsprintf(char *buf, const char *fmt, va_list args)
  */
 int vsnprintf(char *buf, size_t size, const char *fmt, va_list args)
 {
-    unsigned long long num;
-    char              *str, *end;
-    struct printf_spec spec = { 0 };
+    char *str, *end;
 
-    if (size > INT_MAX) {
-        return 0;
-    }
+    unsigned long long num;
+    struct printf_spec spec = { 0 };
 
     str = buf;
     end = buf + size;
