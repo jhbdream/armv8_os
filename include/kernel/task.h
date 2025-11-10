7526471ff2fa4587f2229e1d320aeaa52d15eedc
Hongbin Ji <jhb_ee@163.com>
2025-11-10 18:03:25 +0800

    [task]: init
    Signed-off-by: Hongbin Ji <jhb_ee@163.com>

--- include/kernel/task.h
+++ include/kernel/task.h
@@ -1,31 +1,8 @@
 #ifndef __TASK_H__
 #define __TASK_H__
 
-#include <stdint.h>
+struct task_struct {
 
-#define TASK_NAME_SIZE (32)
-
-typedef enum _task_state {
-    TASK_STATE_FREE,
-    TASK_STATE_INIT,
-    TASK_STATE_READY,
-    TASK_STATE_RUNNING,
-    TASK_STATE_WAIT,
-} task_state;
-
-struct task {
-    void *stack_base;
-    void *stack_top;
-    void *stack_bottom;
-
-    task_state state;
-
-    char name[TASK_NAME_SIZE];
-};
-
-typedef int (*task_func_t)(void *data);
-
-struct task *create_task(char *name, task_func_t func, void *stack_base,
-                         uint32_t stack_size, uint32_t flags);
+} __attribute__((aligned(64)));
 
 #endif // !__TASK_H__
